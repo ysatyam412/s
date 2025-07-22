f05103186287f28c5bdf023a25e68543484f170e
nikhilsainiop <nikhilbhaiop02@gmail.com>
2025-07-22 11:57:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1373,6 +1373,7 @@ async def text_handler(bot: Client, m: Message):
         await editable.delete()
         raw_text2 = "360"
         quality = "360p"
+        res = "640x360"
     else:
         await editable.edit(f"╭━━━━❰ᴇɴᴛᴇʀ ʀᴇꜱᴏʟᴜᴛɪᴏɴ❱━━➣ \n┣━━⪼ send `144`  for 144p\n┣━━⪼ send `240`  for 240p\n┣━━⪼ send `360`  for 360p\n┣━━⪼ send `480`  for 480p\n┣━━⪼ send `720`  for 720p\n┣━━⪼ send `1080` for 1080p\n╰━━⌈⚡[🦋`{CREDIT}`🦋]⚡⌋━━➣ ")
         input2: Message = await bot.listen(editable.chat.id, filters=filters.text & filters.user(m.from_user.id))
